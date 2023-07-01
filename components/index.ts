683c5a5e9795facc15028c5febbec3ba34c88185
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-07-01 12:57:37 -0300

    SearchBar v1

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,17 @@
+import Navbar from "./Navbar";
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
-import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Navbar,
+  Hero,
+  CustomButton,
+  SearchBar,
+  SearchManufacturer,
+  CustomFilter,
+  Footer,
+};

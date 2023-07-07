f310961d3156766a2b5c8844178322a54ffb3870
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-07-07 00:09:14 -0300

    CarImageAPI

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { CustomButton, CarDetails } from "@/components";
 import { CarProps } from "@/types";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 
 interface CardCardProps {
   car: CarProps;

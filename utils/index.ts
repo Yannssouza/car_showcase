82f6b85189159c9800517c35a4d9fa4767756134
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-07-13 19:38:05 -0300

    Custom Filter

--- utils/index.ts
+++ utils/index.ts
@@ -46,3 +46,14 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
   return `${url}`;
 };
+
+export const updateSearchParams = (type: string, value: string) => {
+  // Get the current URL search params
+  const searchParams = new URLSearchParams(window.location.search);
+
+  // Set the specified search parameter to the given value
+  searchParams.set(type, value);
+
+  // Set the specified search parameter to the given value
+  return `${window.location.pathname}?${searchParams.toString()}`;
+};
